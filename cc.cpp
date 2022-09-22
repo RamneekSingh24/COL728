4f84099cf75730a8d40bb35377a2c928ae1a4f13
RamneekSingh24 <ramneeksingh2001@gmail.com>
2022-09-22 22:34:56 +0530

    lab2 part 1

--- cc.cpp
+++ cc.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <iostream>
+#include "ast.h"
 #include "c.tab.hpp"
+
+
 
 extern "C" int yylex();
 int yyparse();
 extern "C" FILE *yyin;
 
+extern yyTU* topLevelTU;
+
 static void usage()
 {
   printf("Usage: cc <prog.c>\n");
 }
+
+using namespace std;
 
 int
 main(int argc, char **argv)
@@ -22,7 +30,19 @@ main(int argc, char **argv)
   char const *filename = argv[1];
   yyin = fopen(filename, "r");
   assert(yyin);
+
   int ret = yyparse();
+
+//  cout << (topLevelTU->nodes.size()) << "\n";
+
+//  for (auto node : topLevelTU->nodes) {
+//      std:: cout << node->name() << "\n";
+//  }
+
+//  std::cout << topLevelTU->nodes[4]->nodes[1]->nodes[1]->nodes[0]->nodes[1]->nodes[0]->name() << "\n";
+
+  topLevelTU->print();
+
   printf("retv = %d\n", ret);
   exit(0);
 }
